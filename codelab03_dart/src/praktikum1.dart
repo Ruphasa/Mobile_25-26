ba9cda4448c9254302ba060f00383cc4ece8afde
ruphasa <rfrizqifauzan@gmail.com>
2025-09-11 14:44:36 +0700

    Feat : Praktikum 1 done

---
+++ codelab03_dart/src/praktikum1.dart
@@ -0,0 +1,20 @@
+import 'dart:math';
+
+void main () {
+  String test = "test2";
+if (test == "test1") {
+  print("Test1");
+} else if (test == "test2") {
+  print("Test2");
+} else {
+  print("Something else");
+}
+
+if (test == "test2") print("Test2 again");
+
+bool testing = true;
+if (testing) {
+  print("Kebenaran");
+}
+
+}
